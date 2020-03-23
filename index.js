5d6333d6af06321c098a10ff9bb24be5f4d772e3
Karunakar Medamoni <kannaiah.yadav14@outlook.com>
2020-03-23 14:46:44 +0530

    octet stream parser changes

--- index.js
+++ index.js
@@ -4,10 +4,11 @@ const multer = require('multer');
 const bodyParser = require('body-parser');
 const xml2js = require('xml2js');
 const fs = require('fs');
-//const unzip = require('unzip2');
+const unzip = require('unzip2');
 
 
 const appPort = 3000;
+const resource_path = __dirname+'/uploads';
 
 const app = express();
 app.use(bodyParser.json());
@@ -30,77 +31,62 @@ const storage = multer.diskStorage({
     }
   });
 
-  var getCourseDetailsFromXml = (jsonData) => {
-    if ( jsonData.tincan && jsonData.tincan.activities && 
-        jsonData.tincan.activities.length  &&
-        jsonData.tincan.activities[0].activity[0] ) {
-
-        const activity = jsonData.tincan.activities[0].activity[0];
-        
-        return  {
-            name: activity.name[0]['_'],
-            desc: activity.description[0]['_']
-        };
-
-    }
-    return {
-        
-    };
-};
-
-var createCourse = (filename) => {
-    return xmlToJson(filename+'/tincan.xml');
- };
-
- const xmlToJson = (filename) => {
-    const parser = new xml2js.Parser({
-      attrkey: "ATTR"
-    });
-    
-    return new Promise((resolve, reject) => {
-        fs.readFile(filename, "utf8", (error, data) => {
-            parser.parseString(data, (err, result) => {
-                if(err ===  null) {
-                    resolve(result);
-                } else {
-                    reject(err);
-                }
-            });
-        });
-    });
-  };
-
 var extractFile = (file) => {
     var filename = file.filename;
     try{
         filename = filename.split('.').slice(0, -1).join('.');
     }catch(error) {
     }
-    const dir = './resources/' + filename;
+    const dir = __dirname + '/resources/' + filename;
 
     return new Promise((resolve, reject) => {
-        fs.createReadStream('./'+file.path).pipe(unzip.Extract({path: dir}))
+        fs.createReadStream(file.path).pipe(unzip.Extract({path: dir}))
         .on('close', () => {
-            if(fs.existsSync(dir)) {
-                createCourse(dir).then((result) => {
-                    result.path = dir;
-                    resolve(result);
-                }, (error) => {
-                    reject(error);
-                });
-            }
         });
     });
 };
 
 
+var octetStreamParser =  bodyParser.raw({
+    inflate: true,
+    limit: '5gb',
+    type: 'application/octet-stream',
+    verify: function(req, res, buf, encoding) {
+      req.rawBody = buf.toString(encoding || 'utf-8');
+  
+  }
+  });
+
+app.get('/',function(req,res){
+    res.sendFile(__dirname + "/public/index.html");
+});
+
+
 // register endpoints quickly to test
 
-app.get('/api/test/connection', (req, res) => {
+app.get('/lrs/api/test/connection', (req, res) => {
     console.log(' filename '+ req);
     res.json({name: 'Karunakar Medamoni '});
 });
 
+/**
+ * Play content from resource folder 
+ */
+app.use('/resources',express.static('resources'));
+
+/**
+ *  Convert Octet Stream into JSON format.
+ *  Send rawBody which was converted by octetStreamParser function.
+ */
+
+app.post('/lrs/api/octet/parser', octetStreamParser, (req, res, next) => {
+    return res.send({body:req.rawBody});
+});
+
+
+/**
+ * File upload and extract into resource folder. 
+ */
 app.post('/lrs/api/file/upload', uploadFile.single('file'), (req, res, next) => {
     console.log(' Inside of file upload ');
     var finalError = new Error("File upload failed ");
@@ -110,22 +96,16 @@ app.post('/lrs/api/file/upload', uploadFile.single('file'), (req, res, next) => 
             return next(error);
         }
         try {
+            // add logic to create folder structure.. Send course number and version number.
             extractFile(req.file).then((data) => {
-                var courseData = getCourseDetailsFromXml(data);
-              
-                  /*  'course_id': req.body.courseid,
-                    'path': data.path,
-                    'name': courseData.name,
-                    'description': courseData.desc,
-
-                    */
-               // return res.send(data); 
+               return res.send(data); 
             }, (error) => {
                 return next(error);
-            });
+            }); 
         }catch(error) {
             return next(finalError);
-        }   
+        }    
+        return res.send({});
 });
 
 app.listen(appPort, () => {
